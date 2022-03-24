1d553435cd99cd2a527917092d31703ee48adcec
sarlac <sachimegawe@gmail.com>
2022-03-24 21:44:52 +0530

    numeric data type

---
+++ numericdatatype.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+float mynum=5.75;
+double mydnum=19.99;
+bool isit=true;
+bool itis=false;
+char mygrade= 'A';
+char c = 67;
+string greeting = "Hello all!";
+
+cout << "\n float number" << mynum;
+cout << "\n double number" << mydnum;
+cout << "\n boolean true" << isit;
+cout << "\n boolean false" << itis;
+cout << "\n character" << mygrade;
+cout << "\n character c" << c;
+cout << "\n greeting";
+
+return 0;
+}
