25e020b81fc6dc6723e980fb349e112d8a02a5ed
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-03-26 22:28:23 +0530

    Basic Arithmatic operations
    Addition, Subtraction, Division, Multiplication, Modulus

---
+++ basicarithmatic.cpp
@@ -0,0 +1,18 @@
+// program to do basic arithmatic operations
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int x,y;
+x=10;
+y=20;
+cout << "\n Addition :" << x+y;
+cout << "\n Subtraction :" << y-x;
+cout << "\n Multiplication :" << x*y;
+cout << "\n Division :" << y/x;
+cout << "\n Modulus :" << x/y;
+
+return (0);
+}
