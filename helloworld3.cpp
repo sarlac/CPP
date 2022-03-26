2302f713825e31c38bed7fa49061edac11009f69
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-03-26 21:28:48 +0530

    program to print hello world 5 times
     in single line using new line character "\n"

---
+++ helloworld3.cpp
@@ -0,0 +1,16 @@
+// Program to print "hello world" 5 times in single line using "\n"
+
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+cout << "\n Hello world !";
+cout << "\n Hello world !";
+cout << "\n Hello world !";
+cout << "\n Hello world !";
+cout << "\n Hello world !";
+
+return (0);
+}
