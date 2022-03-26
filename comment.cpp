eb05eaf31557d3895814ed2cb5964c57b5e88950
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-03-26 21:41:22 +0530

    program to illustrate comment

---
+++ comment.cpp
@@ -0,0 +1,11 @@
+//Program to write "hello world" 10 times in single line using endl
+
+#include<iostream>                  // including iostream library to the program
+using namespace std;                //using namespace std
+
+int main()                          // main function is the important part of this program
+{                                   // open curly bracket to start writing code
+cout << "Hello World !" << endl;   // cout function is used to print text on the output window
+
+return (0);                         // return() function return the value to main function
+}                                   // closing bracket is the end of the program
