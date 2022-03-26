b97c1edc6f61ec3221d60c883ddf5be79b1af373
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-03-26 21:34:27 +0530

    program to print "Hello World!"
    in single line using endl

---
+++ endl.cpp
@@ -0,0 +1,20 @@
+//Program to write "hello world" 10 times in single line using endl
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+cout << "Hello World !" << endl;
+cout << "Hello World !" << endl;
+cout << "Hello World !" << endl;
+cout << "Hello World !" << endl;
+cout << "Hello World !" << endl;
+cout << "Hello World !" << endl;
+cout << "Hello World !" << endl;
+cout << "Hello World !" << endl;
+cout << "Hello World !" << endl;
+cout << "Hello World !" << endl;
+
+return (0);
+}
