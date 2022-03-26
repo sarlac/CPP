0ba0d977b580da0f2e64012f7e04779392fe9b15
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-03-26 21:23:22 +0530

    program to print hello world 5 times in a line

---
+++ hellloworld2.cpp
@@ -0,0 +1,15 @@
+//Program to print "hello world" 5 times in one line
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+cout << "Hello WOrld! ";
+cout << "Hello WOrld! ";
+cout << "Hello WOrld! ";
+cout << "Hello WOrld! ";
+cout << "Hello WOrld! ";
+
+return (0);
+}
