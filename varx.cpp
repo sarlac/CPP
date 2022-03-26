27b8d3bc5288674a6212e246ef858000ff0f5adb
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-03-26 22:04:07 +0530

    Program for varriable declaration

---
+++ varx.cpp
@@ -0,0 +1,13 @@
+//Program to declare one varriable int and assign it any value and print that value using cout
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int x;
+cin >> x;
+cout << "int value :" << x;
+
+return (0);
+}
