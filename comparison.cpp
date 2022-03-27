1f703ae95b4e52d882f64acf80d0924bc4df9552
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-03-27 16:53:38 +0530

    program for all comparison operations

---
+++ comparison.cpp
@@ -0,0 +1,16 @@
+//program to do all comparison operations
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int x = 5;
+int y = 3;
+cout << " X>Y =" <<(x > y);    // returns 1 (true) because 5 is greater than 3
+cout << "\n X==Y =" <<(x == y);
+cout << "\n X!=Y =" <<(x != y);
+cout << "\n X<Y =" <<(x < y);
+cout << "\n X<=Y =" <<(x <= y);
+cout << "\n X>=Y =" <<(x >= y);
+return (0);
+}
