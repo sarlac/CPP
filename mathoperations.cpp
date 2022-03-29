e2ba357ec6baf19efc0bd461f55d19a8e12b0c85
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-03-29 14:13:54 +0530

    program for all math operations

---
+++ mathoperations.cpp
@@ -0,0 +1,17 @@
+//program to illustrate all the math operations
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+cout << "\n Maximum number = " << max(5,10);                    // display maximum number from x,y
+cout << "\n Minimum number = " << min(5,6);                     // display minimum number from x,y
+cout << "\n Sqrt of 16 = " << sqrt(16);                         // display square root value of 16
+cout << "\n Round value of 2.6 = " << round(2.6);               // display round up value of 2.6
+cout << "\n log value of 2 = " <<log(2);                        // display log value of 2
+cout << "\n nearest roundup value of 6.58 = " << floor(6.58);   //display nearest integer value
+cout << "\n";
+return (0);
+}
