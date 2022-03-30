4824be4211440e1fd3e3189efa0da39cf35eabd1
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-03-30 12:13:08 +0530

    swapping 2 numbers using third number

---
+++ swapping.cpp
@@ -0,0 +1,24 @@
+ //Program for swapping two numbers
+
+ #include <iostream>
+ using namespace std;
+
+ int main ()
+ {
+ int a=10;
+ int b=20;
+ cout << "\n value of a=" << a;
+ cout << "\n Value of b= " <<b;
+ int temp;
+ temp=a;
+ a=b;
+ b=temp;
+ cout << "\n\n Values of a and b after swapping\n";
+
+ cout << "\n Value of a=" <<a;
+ cout << "\n Value of b=" <<b;
+ cout <<"\n";
+
+ return 0;
+
+ }
