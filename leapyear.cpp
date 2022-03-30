c2dac5241964061a722cf96b065316c78297dd04
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-03-30 21:59:14 +0530

    program to check leap year

---
+++ leapyear.cpp
@@ -0,0 +1,31 @@
+//Program to check leap year
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+int year;
+cout << "Enter a year :" ;
+cin >> year;
+
+//leap year is perfectly divisible by 400
+if(year%400==0)
+    cout << year <<" is leap year";
+
+//not a leap year if divisible by 100
+//but not divisible by 400
+else if(year%100==0)
+    cout << year << " is not a leap year";
+
+//leap year if not divisible by 4
+//leap year if divisible by 4
+else if(year%4==0)
+    cout <<year << " is a leap year";
+
+//all other years are not leap years
+else
+    cout << year << " is not a leap year";
+
+return 0;
+}
