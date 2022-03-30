618ef0946d2ae7e18b8c982dac574d6354121d0a
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-03-30 13:20:55 +0530

    program to find size of int, char, double, float

---
+++ sizeofintchar.cpp
@@ -0,0 +1,15 @@
+//program to display size of char, float, int, double
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+cout << "\n Size of Char is " << sizeof(char) << " byte" <<endl;
+cout <<" \n Size of int is " << sizeof(int) << " bytes" << endl;
+cout <<" \n Size of float is " << sizeof(float) << " bytes" <<endl;
+cout <<" \n size of double is " << sizeof(double) << " bytes" <<endl;
+
+return 0;
+}
